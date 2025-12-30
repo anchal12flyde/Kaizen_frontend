083d22df6b67aff73b8fc9096b2252087f5380d7
Abhishek <abhishek@flyde.in>
2025-12-30 19:06:32 +0530

    hamburger added.3

--- src/components/ui-kit/spacing.jsx
+++ src/components/ui-kit/spacing.jsx
@@ -2,22 +2,11 @@ import clsx from "clsx";
 
 export const Container = ({ children, variant = "primary", className }) => {
   const variants = {
-    primary: "primary-spacing",
     header: "header-spacing",
     heroSpacing: "hero-spacing",
-    secondary: "secondary-spacing",
-    section: "section-spacing",
-    header : "header-spacing",
-    normal : "normal-spacing",
-    auth : "auth-spacing",
     sectionSp1 : 'section-sp-1',
     sectionSpTop : 'section-sp-top',
     sectionSpBottom : 'section-sp-bottom',
-    topSpacing : "top-spacing",
-    bottomSpacing : "bottom-spacing",
-    blockSpacing : "block-spacing",
-    inlineSpacing : "inline-spacing",
-    blogSpacing : "blog-spacing",
     footerSpacing : "footer-spacing"
   };
 
